3839d33c3c5a71084d436aac325fe2e7f51b9de6
Piyush Baldaniya <piyushbaldaniya888@gmail.com>
2025-02-14 02:11:36 -0500

    added redux

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addToCart, removeFromCart } from "../redux/slices/cartSlice";
+
 
 const ProductDetail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-
+  const dispatch = useDispatch();
   useEffect(() => {
     axios.get(`https://fakestoreapi.com/products/${id}`)
       .then(response => setProduct(response.data))
@@ -15,11 +18,27 @@ const ProductDetail = () => {
   if (!product) return <p>Loading...</p>;
 
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold">{product.title}</h2>
-      <img src={product.image} alt={product.title} className="w-96 h-64 object-cover" />
-      <p className="text-lg">{product.description}</p>
-      <p className="text-xl font-bold">${product.price}</p>
+
+    <div class="flex flex-row">
+      <div class="basis-1/3">
+        <img src={product.image} alt={product.title} className="w-150 h-150 object-cover" />
+      </div>
+      <div class="basis-2/3">
+        <h2 className="text-2xl font-bold text-left">{product.title}</h2>     
+        <p className="text-lg text-left ">{product.description}</p>
+        <p className="text-xl font-bold  text-left ">${product.price}</p>
+        <div class="flex flex-row">
+                <button onClick={() => dispatch(addToCart(product))}
+                className="bg-blue-500 text-white px-4 py-2 rounded">
+                Add to Cart</button>
+
+                <button onClick={() => dispatch(removeFromCart(product))}
+                className="bg-blue-500 text-white px-4 py-2 rounded">
+                Remove From Cart</button>
+        </div>
+      
+
+      </div>
     </div>
   );
 };
